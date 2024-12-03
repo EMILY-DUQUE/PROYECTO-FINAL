37e4b62a815e93517e9edf5f2a855d637088f375
EMILY-DUQUE <emily.duque00@usc.edu.co>
2024-12-02 21:35:15 -0500

    Create Artistas.java

---
+++ Artsitas.java
@@ -0,0 +1,11 @@
+public class Artista {
+    String nombre;
+
+    public Artista(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+}
